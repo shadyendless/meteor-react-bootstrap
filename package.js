0b5bc4dacc72eb9cd1ebe9e06ff968eb9ec979b9
Jacob Foster <dalindeck@gmail.com>
2015-10-16 10:16:37 -0700

    Version update.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'shadyendless:react-bootstrap',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'A Meteor wrapper for the React-Bootstrap project.',
   // URL to the Git repository containing the source code for this package.
